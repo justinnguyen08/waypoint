7da8d84a060a8a08ae59d505b92985d4687e2e3f
Tony Ngo <tonyngo@utexas.edu>
2025-03-28 15:49:17 -0500

    fixed creating account without valid email or password

--- Waypoint/Login and Create Account/CreateAccountViewController.swift
+++ Waypoint/Login and Create Account/CreateAccountViewController.swift
@@ -37,7 +37,12 @@ class CreateAccountViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func createAccountButtonPressed(_ sender: Any) {
-        
+        if (isValidEmail(emailTextField.text!) && isValidPassword(passwordTextField.text!)){
+            performSegue(withIdentifier: "AdditionalInfoSegue", sender: self)
+        }
+        else{
+            self.errorLabel.text = "Invalid email or password"
+        }
     }
     
     // Called when 'return' key pressed
@@ -57,13 +62,8 @@ class CreateAccountViewController: UIViewController, UITextFieldDelegate {
         // Pass the selected object to the new view controller.
         if segue.identifier == "AdditionalInfoSegue",
            let nextVC = segue.destination as? CreateAccountAdditionalInfoViewController{
-            if (isValidEmail(emailTextField.text!) && isValidPassword(passwordTextField.text!)){
                 nextVC.validEmail = emailTextField.text!
                 nextVC.validPassword = passwordTextField.text!
-            }
-            else{
-                self.errorLabel.text = "Invalid email or password"
-            }
         }
     }
     
