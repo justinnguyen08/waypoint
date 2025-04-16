95be5a0077791d36f9b8986bf0fe7d8db8da00c0
Tony Ngo <tonyngo@utexas.edu>
2025-04-15 20:43:44 -0500

    created photoManager and made dailyChallenge work with it

--- Waypoint/Challenges/ChallengesViewController.swift
+++ Waypoint/Challenges/ChallengesViewController.swift
@@ -22,7 +22,6 @@ class ChallengesViewController: UIViewController, UITableViewDelegate, UITableVi
     @IBOutlet weak var weeklyChallengeScore: UILabel!
     @IBOutlet weak var monthlyChallengeScore: UILabel!
     @IBOutlet weak var pointsLabel: UILabel!
-    @IBOutlet weak var dailyChallengeImage: UIImageView!
     @IBOutlet weak var flashButton: UIButton!
     @IBOutlet weak var cameraButton: UIButton!
     @IBOutlet weak var sendButton: UIButton!
@@ -31,17 +30,10 @@ class ChallengesViewController: UIViewController, UITableViewDelegate, UITableVi
     @IBOutlet weak var monthlyView: UIView!
     @IBOutlet weak var monthlyTableView: UITableView!
     @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var cameraDisplayView: UIView!
     
     // camera information
-    var session: AVCaptureSession?
-    var device: AVCaptureDevice?
-    var preview: AVCaptureVideoPreviewLayer?
-    var position: AVCaptureDevice.Position = .back
-    var photoOutput: AVCapturePhotoOutput?
-    var stillImageView: UIImageView?
     var capturedData: Data?
-    var validPicture = false
-    var flashMode: AVCaptureDevice.FlashMode = .off
     var timestamp: Date?
     
     // challanges info
@@ -54,12 +46,29 @@ class ChallengesViewController: UIViewController, UITableViewDelegate, UITableVi
     // allows us access into the Google Firebase Firestore
     let db = Firestore.firestore()
     
+    let photoManager = ChallengePhotoManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         monthlyTableView.delegate = self
         monthlyTableView.dataSource = self
         hideAllButtons()
+        
+        photoManager.onImageCaptured = {
+            image, imageData in
+            self.capturedData = imageData
+            self.timestamp = Date()
+            
+            self.cameraDisplayView.subviews.forEach { $0.removeFromSuperview() }
+            let imageView = UIImageView(frame: self.cameraDisplayView.bounds)
+            imageView.image = image
+            imageView.contentMode = .scaleAspectFill
+            imageView.clipsToBounds = true
+            self.cameraDisplayView.addSubview(imageView)
+            
+            self.showAfterCameraTakenButtons()
+        }
+        
     }
     
     // when the view appears do different things depending o nthe index
@@ -87,8 +96,10 @@ class ChallengesViewController: UIViewController, UITableViewDelegate, UITableVi
                         self.hideAllButtons()
                     }
                     else{
+                        self.dailyView.isHidden = false
                         self.showCameraButtons()
-                        self.setupCaptureSession(with: .back)
+//                        self.setupCaptureSession(with: .back)
+                        self.photoManager.setupCaptureSession(with: .back, view: self.cameraDisplayView)
                     }
                 }
             }
@@ -97,7 +108,8 @@ class ChallengesViewController: UIViewController, UITableViewDelegate, UITableVi
     
     // ensure that if we ever leave then we do not need the camera running anymore
     override func viewDidDisappear(_ animated: Bool) {
-        dismissCamera()
+//        dismissCamera()
+        photoManager.dismissCamera()
     }
     
     // checks to see if the given date is today or not.
@@ -220,9 +232,13 @@ class ChallengesViewController: UIViewController, UITableViewDelegate, UITableVi
                 print("Error fetching daily challenge photo: \(error.localizedDescription)")
                 return
             }
-            if let data = data, let image = UIImage(data: data) {
-                self?.dailyChallengeImage.image = image
-                self?.dailyView.isHidden = false
+            if let data = data, let image = UIImage(data: data), let self = self {
+                cameraDisplayView.subviews.forEach { $0.removeFromSuperview() }
+                let imageView = UIImageView(frame: self.cameraDisplayView.bounds)
+                imageView.image = image
+                imageView.contentMode = .scaleAspectFill
+                self.cameraDisplayView.addSubview(imageView)
+                self.dailyView.isHidden = false
             }
         }
     }
@@ -304,65 +320,9 @@ class ChallengesViewController: UIViewController, UITableViewDelegate, UITableVi
         }
     }
     
-    // we do not need the camera running anymore
-    func dismissCamera(){
-        session?.stopRunning()
-        session = nil
-        preview?.removeFromSuperlayer()
-        preview = nil
-    }
-    
-    // Start live camera session, request access to camera if needed
-    func setupCaptureSession(with position: AVCaptureDevice.Position) {
-        dailyView.isHidden = false
-        guard let newDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: position) else {
-            print("Camera unavailable for position \(position.rawValue)")
-            return
-        }
-        self.device = newDevice
-        do {
-            let input = try AVCaptureDeviceInput(device: self.device!)
-            self.session = AVCaptureSession()
-            session!.sessionPreset = .photo
-
-            if session!.canAddInput(input) {
-                session!.addInput(input)
-            } else {
-                print("Cannot add input to session")
-                return
-            }
-
-            self.preview = AVCaptureVideoPreviewLayer(session: session!)
-            preview!.videoGravity = .resizeAspectFill
-            
-            let previewWidth: CGFloat = view.safeAreaLayoutGuide.layoutFrame.width
-            let previewHeight: CGFloat = 480
-            
-            let xOffset = view.safeAreaLayoutGuide.layoutFrame.minX
-            let yOffset = dailyView.frame.minY + 50
-            
-            preview!.frame =  CGRect(x: xOffset, y: yOffset, width: previewWidth, height: previewHeight)
-            
-            view.layer.addSublayer(preview!)
-            
-            self.photoOutput = AVCapturePhotoOutput()
-            if session!.canAddOutput(photoOutput!) {
-                session!.addOutput(photoOutput!)
-            } else {
-                print("Cannot add photo output to session")
-                return
-            }
-
-            session!.startRunning()
-        } catch {
-            print("Unable to create input: \(error.localizedDescription)")
-        }
-    }
-    
     // turn the flash on or off for the camera
     @IBAction func flashButtonPressed(_ sender: Any) {
-        self.flashMode = (self.flashMode == .off) ? .on : .off
-        if self.flashMode == .on{
+        if photoManager.toggleFlash(){
             self.flashButton.setImage(UIImage(systemName: "flashlight.on.fill"), for: .normal)
         }
         else{
@@ -372,119 +332,18 @@ class ChallengesViewController: UIViewController, UITableViewDelegate, UITableVi
     
     // flip from the back or front camera
     @IBAction func flipCamera(_ sender: Any) {
-        guard let currentSession = session, currentSession.isRunning else { return }
-        
-        let newPosition: AVCaptureDevice.Position = (position == .front) ? .back : .front
-        
-        currentSession.beginConfiguration()
-        
-        // Stopping active camera
-        if let currentInput = currentSession.inputs.first as? AVCaptureDeviceInput {
-            currentSession.removeInput(currentInput)
-        }
-        
-        // Setting up device
-        guard let newDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: newPosition) else {
-            print("Camera unavailable for position \(newPosition.rawValue)")
-            currentSession.commitConfiguration()
-            return
-        }
-        
-        // Add to session
-        do {
-            let newInput = try AVCaptureDeviceInput(device: newDevice)
-            if currentSession.canAddInput(newInput) {
-                currentSession.addInput(newInput)
-                self.device = newDevice
-                self.position = newPosition
-            } else {
-                print("Cannot add new input to session")
-            }
-        } catch {
-            print("Error creating new input: \(error.localizedDescription)")
-        }
-        
-        currentSession.commitConfiguration()
+        photoManager.flipCamera()
     }
     
     // actually take the picture
     @IBAction func capturePicture(_ sender: UIButton) {
-        guard let photoOutput = photoOutput else { return }
-        let settings = AVCapturePhotoSettings()
-        settings.flashMode = self.flashMode
-        photoOutput.capturePhoto(with: settings, delegate: self)
-        validPicture = true
-        self.showAfterCameraTakenButtons()
-    }
-    
-    // Saves still image of captured photo
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        if let error = error {
-            print("Error capturing photo: \(error.localizedDescription)")
-            return
-        }
-        
-        guard let imageData = photo.fileDataRepresentation() else { return }
-        guard var image = UIImage(data: imageData) else { return }
-        
-        timestamp = Date()
-        
-        if let exifMeta = photo.metadata[kCGImagePropertyExifDictionary as String] as? [String: Any],
-           let originalDate = exifMeta[kCGImagePropertyExifDateTimeOriginal as String] as? String {
-            print("EXIF Timestamp: \(originalDate)")
-        } else {
-            print("Using current timestamp: \(timestamp)")
-        }
-        if self.position == .front {
-            guard let cgImage = image.cgImage else { return }
-            image = UIImage(cgImage: cgImage, scale: image.scale, orientation: .leftMirrored)
-        }
-        
-        // update interface with image, setting photo's properties
-        DispatchQueue.main.async {
-            self.session?.stopRunning()
-            self.preview?.removeFromSuperlayer()
-            
-            if self.stillImageView == nil {
-                let previewWidth: CGFloat = self.view.safeAreaLayoutGuide.layoutFrame.width
-                let previewHeight: CGFloat = 480
-                
-                let xOffset = self.view.safeAreaLayoutGuide.layoutFrame.minX
-                let yOffset = self.dailyView.frame.minY + 50
-                
-                self.stillImageView = UIImageView(frame: CGRect(x: xOffset, y: yOffset, width: previewWidth, height: previewHeight))
-                self.stillImageView!.contentMode = .scaleAspectFill
-                self.stillImageView!.clipsToBounds = true
-                self.view.addSubview(self.stillImageView!)
-                self.backButton.removeFromSuperview()
-                self.view.addSubview(self.backButton)
-                
-                if let stillImageView = self.stillImageView {
-                    let padding: CGFloat = 10
-                    self.backButton.frame = CGRect(
-                        x: stillImageView.frame.minX + padding,
-                        y: stillImageView.frame.minY + padding,
-                        width: self.backButton.frame.width,
-                        height: self.backButton.frame.height
-                    )
-                }
-            }
-            
-            self.stillImageView!.image = image
-            self.showAfterCameraTakenButtons()
-            self.capturedData = imageData
-        }
+        photoManager.capturePhoto()
     }
     
     // actually upload the photo and update necessary information
     @IBAction func sendPhotoButtonPressed(_ sender: Any) {
-        print("sendPhotoPressed")
         guard capturedData != nil else {
             print("No image data to upload")
-            return
-        }
-        if (!validPicture) {
-            print("Picture already uploaded")
             return
         }
         
@@ -503,18 +362,17 @@ class ChallengesViewController: UIViewController, UITableViewDelegate, UITableVi
                 else{
                     
                     self.uploadImage(imageData: self.capturedData!)
-                    self.dismissCamera()
+                    self.photoManager.dismissCamera()
                     // update the UI
                     DispatchQueue.main.async {
-                        self.stillImageView?.removeFromSuperview()
-                        self.stillImageView = nil
                         if let imageData = self.capturedData, let image = UIImage(data: imageData) {
-                            self.dailyChallengeImage.image = image
-                            self.dailyChallengeImage.isHidden = false
+                            self.cameraDisplayView.layer.contents = image
+                            self.cameraDisplayView.contentMode = .scaleAspectFill
+                            self.cameraDisplayView.clipsToBounds = true
+                            self.cameraDisplayView.isHidden = false
                         }
                         self.hideAllButtons()
                     }
-                    self.validPicture = false
                 }
             }
         }
@@ -528,11 +386,8 @@ class ChallengesViewController: UIViewController, UITableViewDelegate, UITableVi
     
     // resume the camera session
     func resumeCamera(){
-        stillImageView?.removeFromSuperview()
-        stillImageView = nil
+        photoManager.setupCaptureSession(with: .back, view: cameraDisplayView)
         showCameraButtons()
-        setupCaptureSession(with: position)
-        validPicture = false
         capturedData = nil
     }
     
@@ -653,7 +508,7 @@ class ChallengesViewController: UIViewController, UITableViewDelegate, UITableVi
                     }
                     else{
                         self.showCameraButtons()
-                        self.setupCaptureSession(with: .back)
+                        self.photoManager.setupCaptureSession(with: .back, view: self.cameraDisplayView)
                     }
                 }
             }
@@ -661,7 +516,7 @@ class ChallengesViewController: UIViewController, UITableViewDelegate, UITableVi
             monthlyView.isHidden = true
             view.bringSubviewToFront(dailyView)
         case 1: // monthly view
-            dismissCamera()
+            photoManager.dismissCamera()
             monthlyView.isHidden = false
             dailyView.isHidden = true
             loadChallenges {
@@ -669,7 +524,7 @@ class ChallengesViewController: UIViewController, UITableViewDelegate, UITableVi
             }
             view.bringSubviewToFront(monthlyView)
         case 2: // segue to feed page
-            dismissCamera()
+            photoManager.dismissCamera()
             monthlyView.isHidden = true
             dailyView.isHidden = true
             performSegue(withIdentifier: "ChallengeFeedSegue", sender: self)
